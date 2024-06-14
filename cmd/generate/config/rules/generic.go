dab10f8d33bd1ac9c679364d4ee0ae389b4353a8
Sugandasree <107237480+Sugayuv1@users.noreply.github.com>
2024-06-14 11:40:32 +0530

    Update generic.go

--- cmd/generate/config/rules/generic.go
+++ cmd/generate/config/rules/generic.go
@@ -42,7 +42,7 @@ func GenericCredential() *config.Rule {
 		generateSampleSecret("generic", "CLOJARS_34bf0e88955ff5a1c328d6a7491acc4f48e865a7b8dd4d70a70749037443"),
 		generateSampleSecret("generic", "Zf3D0LXCM3EIMbgJpUNnkRtOfOueHznB"),
 
-		`"client_secret" : "6da89121079f83b2eb6acccf8219ea982c3d79bccc3e9c6a85856480661f8fde",`,
+
 	}
 	fps := []string{
 		`client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client-vpn-endpoint.id`,
